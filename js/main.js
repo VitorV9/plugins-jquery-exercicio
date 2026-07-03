d67b3267e669693a9243ff45894ac4e3a10c3a0f
VitorVT9 <vsilvab57@gmail.com>
2026-07-03 20:04:09 -0300

    fix: corrige id incorreto atribuido a função slick

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+ $(document).ready(function () {
+            $('#carousel-imagens').slick({
+                autoplay: true,
+                arrows: false
+            })
+        })
+        
 $('#telefone').mascara('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
 })
